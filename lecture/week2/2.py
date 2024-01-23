05af5de94e0886bebeff61a746da2f92c374da6c
Beisenbek M. Baisakov <b.baisakov@kbtu.kz>
2024-01-23 19:54:18 +0600

    'upd'

---
+++ lecture/week2/2.py
@@ -0,0 +1,2 @@
+x = set({})
+print(type(x))
---
+++ lecture/week2/3.py
@@ -0,0 +1,8 @@
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+print(type(red))
