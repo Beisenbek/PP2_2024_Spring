69bcd7f36a96e5b6060a75fd829504e664fca836
Beisenbek M. Baisakov <b.baisakov@kbtu.kz>
2024-03-12 18:55:06 +0500

    'upd'

--- lecture/week7/7.py
+++ lecture/week7/7.py
@@ -2,45 +2,74 @@ from msvcrt import getch
 from colorama import Fore, Back, Style
 import os 
 
-activeItemIndex = 1
 
-def draw_layer(path):
-    global activeItemIndex
-    global currentItemIndex
-    currentItemIndex = 0
+class Layer:
+    def __init__(self, path):
+        self.activeItemIndex = 0
+        self.path = path
+        self.entries = []
+        self.fill_entries()
 
-    with os.scandir(path) as it:
-        for entry in it:
-            if(entry.is_dir()):
-                if activeItemIndex == currentItemIndex:
-                    print(Back.GREEN + entry.name, end = '')
-                else:
-                    print(Back.BLACK + entry.name, end = '')
-                currentItemIndex = currentItemIndex + 1
-                print(Back.BLACK + '')
+    def fill_entries(self):
+        with os.scandir(self.path) as it:
+            for entry in it:
+                if(entry.is_dir()):
+                    self.entries.append(entry)
 
-    with os.scandir(path) as it:
-        for entry in it:
-            if(not entry.is_dir()):
-                if activeItemIndex == currentItemIndex:
-                    print(Back.GREEN + entry.name, end = '')
-                else:
-                    print(Back.BLACK + entry.name, end = '')
-                currentItemIndex = currentItemIndex + 1
-                print(Back.BLACK + '')
+        with os.scandir(self.path) as it:
+            for entry in it:
+                if(not entry.is_dir()):
+                    self.entries.append(entry)
 
-    print(Style.RESET_ALL)
+    def step(self, dx):
+        self.activeItemIndex += dx
+        if(self.activeItemIndex < 0):
+            self.activeItemIndex = len(self.entries) - 1
+        elif(self.activeItemIndex >= len(self.entries)):
+            self.activeItemIndex = 0
+        
+
+    def draw(self):
+        currentItemIndex = 0
+
+        for entry in self.entries:
+            if self.activeItemIndex == currentItemIndex:
+                print(Back.GREEN + entry.name, end = '')
+            else:
+                print(Back.BLACK + entry.name, end = '')
+
+            currentItemIndex = currentItemIndex + 1
+            print(Back.BLACK + '')
 
 
+        print(Style.RESET_ALL)
+    
+    def open(self):
+        entry = self.entries[self.activeItemIndex]
+        if (entry.is_dir()):
+            return entry.path
+        return None
+
+        
+
+
+layers = [Layer('.')]
 
 while True:
     os.system("cls")
-    draw_layer('.')
+    layers[-1].draw()
     key = ord(getch())
     if key == 27: #ESC
         break
     elif key == 80: #Down arrow
-        activeItemIndex = activeItemIndex + 1
+        layers[-1].step(1)
     elif key == 72: #Up arrow
-        activeItemIndex = activeItemIndex - 1
+        layers[-1].step(-1)
+    elif key == 13: #Enter
+        child_path =  layers[-1].open()
+        if(child_path != None):
+            layers.append(Layer(child_path))
+    elif key == 8: #Backspace
+        layers.pop()
+        
     
